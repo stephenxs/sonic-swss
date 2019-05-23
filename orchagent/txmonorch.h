95506198b01a3b3f13130748a5771ae795e77f5e
Stephen Sun <stephens@arc-build-server-2.mtbc.labs.mlnx>
2019-05-23 05:05:21 +0300

    address comments 1. fix the indent 2. implement SelectableTimer as a member in order to make the code clear 3. store human-readable strings for "tx_status" in TX_ERR_STATE db so that CLI code don't need to convert tx_status from integer to string. 4. add return values for function that handle configuration update.

--- orchagent/txmonorch.h
+++ orchagent/txmonorch.h
@@ -5,11 +5,10 @@
 #include "producerstatetable.h"
 #include "observer.h"
 #include "portsorch.h"
-//#include "neighorch.h"
-//#include "routeorch.h"
-//#include "fdborch.h"
 #include "selectabletimer.h"
 #include "table.h"
+#include "select.h"
+#include "timer.h"
 
 #include <map>
 #include <algorithm>
@@ -21,25 +20,31 @@ extern "C" {
 }
 
 /*fields definition*/
-#define TXMONORCH_FIELD_CFG_PERIOD		"tx_error_check_period"
-#define TXMONORCH_FIELD_CFG_THRESHOLD		"tx_error_threshold"
+#define TXMONORCH_FIELD_CFG_PERIOD      "tx_error_check_period"
+#define TXMONORCH_FIELD_CFG_THRESHOLD       "tx_error_threshold"
 
-#define TXMONORCH_FIELD_APPL_STATI		"tx_error_stati"
-#define TXMONORCH_FIELD_APPL_TIMESTAMP	"tx_error_timestamp"
-#define TXMONORCH_FIELD_APPL_SAIPORTID	"tx_error_portid"
+#define TXMONORCH_FIELD_APPL_STATI      "tx_error_stati"
+#define TXMONORCH_FIELD_APPL_TIMESTAMP  "tx_error_timestamp"
+#define TXMONORCH_FIELD_APPL_SAIPORTID  "tx_error_portid"
 
-#define TXMONORCH_FIELD_STATE_TX_STATE	"tx_status"
+#define TXMONORCH_FIELD_STATE_TX_STATE  "tx_status"
 
 /*key name definition*/
 /*key name for each port is its intf name*/
 /*key name of global period*/
-#define TXMONORCH_KEY_CFG_PERIOD	"GLOBAL_PERIOD" 
+#define TXMONORCH_KEY_CFG_PERIOD    "GLOBAL_PERIOD" 
 
 /*table names are defined in schema.h*/
 
-#define TXMONORCH_ERR_STATE		"tx_status"
+#define TXMONORCH_ERR_STATE     "tx_status"
 
-#define TXMONORCH_SEL_TIMER		"TX_ERR_COUNTERS_POLL"
+#define TXMONORCH_SEL_TIMER     "TX_ERR_COUNTERS_POLL"
+
+/*tx state definition*/
+#define TXMONORCH_PORT_STATE_OK         0
+#define TXMONORCH_PORT_STATE_ERROR      1
+#define TXMONORCH_PORT_STATE_UNKNOWN    2
+#define TXMONORCH_PORT_STATE_MAX        3
 
 typedef std::tuple<int, sai_object_id_t, uint64_t, uint64_t> TxErrorStatistics;//state, stati, threshold
 typedef std::map<std::string, TxErrorStatistics> TxErrorStatMap;
@@ -52,35 +57,36 @@ typedef std::map<std::string, TxErrorStatistics> TxErrorStatMap;
 class TxMonOrch : public Orch
 {
 public:
-	TxMonOrch(TableConnector appDbConnector, 
-			  TableConnector confDbConnector, 
-			  TableConnector stateDbConnector);
+    TxMonOrch(TableConnector appDbConnector, 
+              TableConnector confDbConnector, 
+              TableConnector stateDbConnector);
 
 private:
-	//ProducerStateTable is designed to provide a IPC ability, 
-	//Table is designed for data persistence.
-	//representing PORT_TX_STATISTICS_TABLE in APPL_DB
-	Table m_TxErrorTable;
-	//representing PORT_TX_STAT_TABLE in STATE_DB
-	Table m_stateTxErrorTable;
+    //ProducerStateTable is designed to provide a IPC ability, 
+    //Table is designed for data persistence.
+    //representing PORT_TX_STATISTICS_TABLE in APPL_DB
+    Table m_TxErrorTable;
+    //representing PORT_TX_STAT_TABLE in STATE_DB
+    Table m_stateTxErrorTable;
 
-	//for fetching statistics
+    //for fetching statistics
     DBConnector m_countersDb;
     Table m_countersTable;
 
-	TxErrorStatMap m_PortsTxErrStat;
+    TxErrorStatMap m_PortsTxErrStat;
 
-	/*should be accessed via an atomic approach?*/
-	uint32_t m_pollPeriod;
-	int m_poolPeriodChanged;
+    /*should be accessed via an atomic approach?*/
+    uint32_t m_pollPeriod;
+    int m_poolPeriodChanged;
+    SelectableTimer *m_pollTimer;
 
     void doTask(Consumer& consumer);
     void doTask(SelectableTimer &timer);
 
-	void startTimer(uint32_t interval);
-	void handlePeriodUpdate(const vector<FieldValueTuple>& data);
-	void handleThresholdUpdate(const string &key, const vector<FieldValueTuple>& data, bool clear);
-	int pollOnePortErrorStatistics(const string &port, TxErrorStatistics &stat);
-	void pollErrorStatistics();
+    void startTimer(uint32_t interval);
+    int handlePeriodUpdate(const vector<FieldValueTuple>& data);
+    int handleThresholdUpdate(const string &key, const vector<FieldValueTuple>& data, bool clear);
+    int pollOnePortErrorStatistics(const string &port, TxErrorStatistics &stat);
+    void pollErrorStatistics();
 };
 #endif /* SWSS_TXMONORCH_H */
