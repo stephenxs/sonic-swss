9d372451778c823e3535e38f0d2200621746e8d6
Stephen Sun <stephens@nvidia.com>
2023-08-20 10:37:33 +0000

    Handle a corner case that the scheduling latency is very long
    Signed-off-by: Stephen Sun <stephens@nvidia.com>

--- orchagent/pfc_detect_mellanox.lua
+++ orchagent/pfc_detect_mellanox.lua
@@ -12,6 +12,34 @@ local rets = {}
 
 redis.call('SELECT', counters_db)
 
+-- Handle corner case: the scheduling latency is very long
+-- In case the scheduling latency is very long, the effective poll time should be the scheduling latency
+local timestamp_last = redis.call('HGET', 'TIMESTAMP', 'POLL_TIMESTAMP_last')
+local timestamp_struct = redis.call('TIME')
+local timestamp_current = timestamp_struct[1] + timestamp_struct[2] / 1000000
+local timestamp_string = tostring(timestamp_current)
+redis.call('HSET', 'TIMESTAMP', 'POLL_TIMESTAMP_last', timestamp_string)
+local real_poll_time = poll_time
+local poll_time_adjust_string = redis.call('HGET', 'TIMESTAMP', 'poll_time_adjust')
+local poll_time_adjust = 0
+local new_adjust = 0
+if poll_time_adjust_string ~= false then
+    redis.call('HDEL', 'TIMESTAMP', 'poll_time_adjust')
+    poll_time_adjust = tonumber(poll_time_adjust_string)
+end
+if timestamp_last ~= false then
+    real_poll_time = (timestamp_current - tonumber(timestamp_last)) * 1000000
+    new_adjust = real_poll_time - poll_time
+    real_poll_time = real_poll_time + poll_time_adjust
+    if real_poll_time < poll_time then
+        real_poll_time = poll_time
+    else
+        if new_adjust > 0 then
+            redis.call('HSET', 'TIMESTAMP', 'poll_time_adjust', new_adjust)
+        end
+    end
+end
+
 -- Iterate through each queue
 local n = table.getn(KEYS)
 for i = n, 1, -1 do
@@ -46,8 +74,6 @@ for i = n, 1, -1 do
             local pfc_duration = redis.call('HGET', counters_table_name .. ':' .. port_id, pfc_duration_key)
 
             if occupancy_bytes and packets and pfc_rx_packets and pfc_duration then
-                local check_timestamp_struct = redis.call('TIME')
-                local check_timestamp_string = tostring(check_timestamp_struct[1]) .. '.' .. tostring(check_timestamp_struct[2])
 
                 occupancy_bytes = tonumber(occupancy_bytes)
                 packets = tonumber(packets)
@@ -66,7 +92,7 @@ for i = n, 1, -1 do
                     packets_last = tonumber(packets_last)
                     pfc_rx_packets_last = tonumber(pfc_rx_packets_last)
                     pfc_duration_last = tonumber(pfc_duration_last)
-                    local storm_condition = (pfc_duration - pfc_duration_last) > (poll_time * 0.8)
+                    local storm_condition = (pfc_duration - pfc_duration_last) > (real_poll_time * 0.8)
 
                     -- Check actual condition of queue being in PFC storm
                     if (occupancy_bytes > 0 and packets - packets_last == 0 and pfc_rx_packets - pfc_rx_packets_last > 0) or
@@ -81,8 +107,7 @@ for i = n, 1, -1 do
                             local packets_string = '"packets","' .. tostring(packets) .. '","packets_last","' .. tostring(packets_last) .. '",'
                             local pfc_rx_packets_string = '"pfc_rx_packets","' .. tostring(pfc_rx_packets) .. '","pfc_rx_packets_last","' .. tostring(pfc_rx_packets_last) .. '",'
                             local storm_condition_string = '"pfc_duration","' .. tostring(pfc_duration) .. '","pfc_duration_last","' .. tostring(pfc_duration_last) .. '",'
-                            local timestamp_last = redis.call('HGET', counters_table_name .. ':' .. KEYS[i], 'POLL_TIMESTAMP_last')
-                            local timestamps = '"timestamp","' .. check_timestamp_string .. '","timestamp_last","' .. timestamp_last .. '"'
+                            local timestamps = '"timestamp","' .. timestamp_string .. '","timestamp_last","' .. timestamp_last .. '","real_poll_time","' .. real_poll_time .. '","poll_time_adjust","' .. poll_time_adjust .. '"'
                             redis.call('PUBLISH', 'PFC_WD_ACTION', '["' .. KEYS[i] .. '","storm",' .. occupancy_string .. packets_string .. pfc_rx_packets_string .. storm_condition_string .. timestamps .. ']')
                             is_deadlock = true
                             time_left = detection_time
@@ -100,7 +125,6 @@ for i = n, 1, -1 do
             -- Save values for next run
                 redis.call('HSET', counters_table_name .. ':' .. KEYS[i], 'SAI_QUEUE_STAT_PACKETS_last', packets)
                 redis.call('HSET', counters_table_name .. ':' .. KEYS[i], 'PFC_WD_DETECTION_TIME_LEFT', time_left)
-                redis.call('HSET', counters_table_name .. ':' .. KEYS[i], 'POLL_TIMESTAMP_last', check_timestamp_string)
                 if is_deadlock == false then
                     redis.call('HSET', counters_table_name .. ':' .. port_id, pfc_rx_pkt_key .. '_last', pfc_rx_packets)
                     redis.call('HSET', counters_table_name .. ':' .. port_id, pfc_duration_key .. '_last', pfc_duration)
