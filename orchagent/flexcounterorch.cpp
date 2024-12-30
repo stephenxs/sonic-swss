b2aac8937677992654ae8bf9d7f387b800e2b0d1
Stephen Sun <stephens@nvidia.com>
2024-12-30 02:02:12 +0000

    Flush all counters if flex counter configuration changes
    Signed-off-by: Stephen Sun <stephens@nvidia.com>

--- orchagent/flexcounterorch.cpp
+++ orchagent/flexcounterorch.cpp
@@ -245,6 +245,7 @@ void FlexCounterOrch::doTask(Consumer &consumer)
                         }
                     }
 
+                    gPortsOrch->flushCounters();
                     setFlexCounterGroupOperation(flexCounterGroupMap[key], value);
 
                     if (gPortsOrch && gPortsOrch->isGearboxEnabled())
