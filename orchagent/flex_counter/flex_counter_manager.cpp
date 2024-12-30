86c355796a7706f09ace44f60584e5c89457f352
Stephen Sun <stephens@nvidia.com>
2024-12-30 10:51:02 +0000

    Implement PFC WD flex using counter manager
    Signed-off-by: Stephen Sun <stephens@nvidia.com>

--- orchagent/flex_counter/flex_counter_manager.cpp
+++ orchagent/flex_counter/flex_counter_manager.cpp
@@ -40,6 +40,7 @@ const unordered_map<CounterType, string> FlexCounterManager::counter_id_field_lo
     { CounterType::SWITCH_DEBUG,    SWITCH_DEBUG_COUNTER_ID_LIST },
     { CounterType::PORT,            PORT_COUNTER_ID_LIST },
     { CounterType::QUEUE,           QUEUE_COUNTER_ID_LIST },
+    { CounterType::QUEUE_ATTR,      QUEUE_ATTR_ID_LIST },
     { CounterType::PRIORITY_GROUP,  PG_COUNTER_ID_LIST },
     { CounterType::MACSEC_SA_ATTR,  MACSEC_SA_ATTR_ID_LIST },
     { CounterType::MACSEC_SA,       MACSEC_SA_COUNTER_ID_LIST },
